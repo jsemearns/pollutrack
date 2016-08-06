a26ccfb4a0716423949e21b6719125bf58220201
Paulinko Kiel Labian <paw2geek23@gmail.com>
2016-08-06 18:39:57 +0800

    map marker

--- pollutrack/pollutrack/static/js/script.js
+++ pollutrack/pollutrack/static/js/script.js
@@ -1,7 +1,27 @@
+var MAP;
+var marker = {
+    position: {lat: 10.3267959, lng: 123.9108368}
+};
+
 function initMap() {
     var mapDiv = document.getElementById('map');
-    var map = new google.maps.Map(mapDiv, {
+    MAP = new google.maps.Map(mapDiv, {
         center: {lat: 10.3267959, lng: 123.9108368},
-        zoom: 15
+        zoom: 10
     });
+
+    addAllMarkers([marker]);
+}
+
+function addMarker(source) {
+    var marker = new google.maps.Marker({
+        position: source.position,
+        map: MAP
+    });
+}
+
+function addAllMarkers(sources) {
+    for (var i = 0; i < sources.length; i++) {
+        addMarker(sources[i]);
+    }
 }
