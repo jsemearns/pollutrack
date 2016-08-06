3e4660f4491617e62b31e076b15fd8697a70e9a0
jsemearns <justin.mearns@gmail.com>
2016-08-07 03:21:09 +0800

    added geo functions

--- pollutrack/pollutrack/static/js/script.js
+++ pollutrack/pollutrack/static/js/script.js
@@ -2,6 +2,7 @@ var MAP;
 var marker = {
     position: {lat: 10.3267959, lng: 123.9108368}
 };
+var Geocoder = new google.maps.Geocoder();
 
 var templates = {
     pollutionDetail: $('#pollution-detail-template').html(),
@@ -69,3 +70,31 @@ $('.pollution-list').on('click', 'li.collection-item', function(e) {
     MAP.setCenter(new google.maps.LatLng(elem.data('lat'), elem.data('long')));
     showPollutionInfo(elem.data('pk'));
 });
+
+function getUserPosition() {
+    var startPos;
+    var geoSuccess = function(position) {
+        startPos = position;
+        return { 'lng': startPos.coords.longitude, 'lat': startPos.coords.latitude }
+    };
+    var geoError = function(error) {
+        console.log('Error occurred. Error code: ' + error.code);
+        // error.code can be:
+        //   0: unknown error
+        //   1: permission denied
+        //   2: position unavailable (error response from location provider)
+        //   3: timed out
+        return false;
+    };
+    return navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+}
+
+function getCoordinates(address) {
+  geocoder.geocode({'address': address}, function(results, status) {
+      if (status === 'OK') {
+          return results[0].geometry.location;
+        } else {
+          alert('Geocode was not successful for the following reason: ' + status);
+        }
+      });
+}
