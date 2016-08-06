5edc33a0bbf57824016cb95e8df58b8b7b252cd5
Arnelle Balane <arnellebalane@gmail.com>
2016-08-06 16:24:18 +0800

    now has map

--- pollutrack/pollutrack/settings.py
+++ pollutrack/pollutrack/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'profile',
+    'pollutrack',
 ]
 
 MIDDLEWARE_CLASSES = [
--- pollutrack/pollutrack/static/js/script.js
+++ pollutrack/pollutrack/static/js/script.js
@@ -1,7 +1,7 @@
 function initMap() {
     var mapDiv = document.getElementById('map');
     var map = new google.maps.Map(mapDiv, {
-        center: {lat: 44.540, lng: -78.546},
-        zoom: 8
+        center: {lat: 10.3267959, lng: 123.9108368},
+        zoom: 15
     });
 }
