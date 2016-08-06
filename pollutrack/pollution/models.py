5003b2e8b04835e96e707e5e0dea6ffb0bea3fb7
Emmanuel Lodovice <name3anad@gmail.com>
2016-08-06 19:54:25 +0800

    added images for after place is fixed

--- pollutrack/pollution/models.py
+++ pollutrack/pollution/models.py
@@ -18,7 +18,10 @@ class PollutionSource(models.Model):
     description = models.TextField(blank=True)
     center = models.OneToOneField(Coordinates, related_name='source')
     is_verified = models.BooleanField(default=False)
-    images = models.ManyToManyField(ImageUploads)
+    is_fixed = models.BooleanField(default=False)
+    images = models.ManyToManyField(ImageUploads, related_name='pollutions')
+    after_images = models.ManyToManyField(
+        ImageUploads, related_name='after_pollutions')
     address = models.TextField(blank=True)
     when = models.DateTimeField(auto_now_add=True)
     user_approved = models.ManyToManyField(User, related_name='approved_posts')
